1f915d5518db773b5962a5058678915b2f038aa6
Belle0317 <46584373+Belle0317@users.noreply.github.com>
2019-01-11 10:36:49 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todolist from './Todolist';
+
+//JSX 语法中，如果我们要使用自己创建的组件
+ReactDOM.render(<Todolist />, document.getElementById('root'));
